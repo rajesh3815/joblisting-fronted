a95b4224b7019df93e45553f847232034fcd8cc3
Rajesh Kumar <rkraja730@gmail.com>
2024-03-29 15:30:27 +0530

    netlify bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import SingnupPage from "./pages/signinpage/SingnupPage";
 import JobPost from "./pages/jobpost/JobPost";
 import Jobdetails from "./pages/jobdetails/Jobdetails";
 import HomePage from "./pages/home/HomePage";
+import Notfound from "./pages/notfound/Notfound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route exact path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SingnupPage />} />
         <Route path="/job-post" element={<JobPost />} />
         <Route path="/job-details/:id" element={<Jobdetails />} />
+        <Route  element={<Notfound />}></Route>
       </Routes>
     </BrowserRouter>
   );
